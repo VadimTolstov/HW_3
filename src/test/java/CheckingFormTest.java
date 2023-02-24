827f8b22f3f85e77f92373bc895a44d5c415b3d4
VAdimT <vadimt382@gmail.com>
2023-02-24 11:25:45 +0300

    4l

--- src/test/java/CheckingFormTest.java
+++ src/test/java/CheckingFormTest.java
@@ -14,7 +14,7 @@ public class CheckingFormTest {
     @BeforeAll
     static void beforeAll(){
         Configuration.browserSize = "1920x1080";
-//        Configuration.holdBrowserOpen = true;
+     //   Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
     }
 
@@ -27,7 +27,7 @@ public class CheckingFormTest {
         $("#firstName").setValue("Алексей Иванович");
         $("[id=lastName]").setValue("Ivanov");
         $("#userEmail").setValue("Ivanov@google.com");
-        $("[for=gender-radio-2]").click();
+        $(byText("Female")).click();
         $("#userNumber").setValue("7878787878");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-dropdown-container").$(byText("May")).click();
@@ -35,34 +35,45 @@ public class CheckingFormTest {
         $("[aria-label='Choose Wednesday, May 25th, 1988']").click();
         $("#subjectsInput").setValue("English").pressEnter();
 
-      //  $("#hobbies-checkbox-3").parent().click();
-        $("label[for=hobbies-checkbox-1]").click();
-        $("label[for=hobbies-checkbox-2]").click();
-        $("label[for=hobbies-checkbox-3]").click();
+        $(byText("Music")).click();
+        $(byText("Reading")).click();
+        $(byText("Sports")).click();
+
+
+//        $("label[for=hobbies-checkbox-1]").click();
+//        $("label[for=hobbies-checkbox-2]").click();
+//        $("label[for=hobbies-checkbox-3]").click();
 
         File file = new File("src/test/resources/pictures/JAVA_20.6_10.jpg");
         $("#uploadPicture").uploadFile(file);
 
         $("#currentAddress").setValue("Hello");
         $("#state").click();
-        $("#react-select-3-option-0").click();
+        $(byText("NCR")).click();
         $("#city").click();
-        $("#react-select-4-option-0").click();
+        $(byText("Delhi")).click();
 
         $("#submit").click();
 
-        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-        $("table tbody tr:nth-child(1) td:nth-child(2)").shouldHave(exactText("Алексей Иванович Ivanov"));
-        $("table tbody tr:nth-child(2) td:nth-child(2)").shouldHave(exactText("Ivanov@google.com"));
-        $("table tbody tr:nth-child(3) td:nth-child(2)").shouldHave(exactText("Female"));
+//        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+//        $("table tbody tr:nth-child(1) td:nth-child(2)").shouldHave(exactText("Алексей Иванович Ivanov"));
+//        $("table tbody tr:nth-child(2) td:nth-child(2)").shouldHave(exactText("Ivanov@google.com"));
+//        $("table tbody tr:nth-child(3) td:nth-child(2)").shouldHave(exactText("Female"));
 
+        $(".modal-content").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-content").shouldHave(text("Алексей Иванович Ivanov"));
+        $(".modal-content").shouldHave(text("Ivanov@google.com"));
+        $(".modal-content").shouldHave(text("Female"));
         $(".modal-content").shouldHave(text("7878787878"));
         $(".modal-content").shouldHave(text("25 May,1988"));
-        $(".modal-content").shouldHave(text("Sports, Reading, Music"));
+        $(".modal-content").shouldHave(text("Music, Reading, Sports"));
         $(".modal-content").shouldHave(text("JAVA_20.6_10.jpg"));
         $(".modal-content").shouldHave(text("Hello"));
         $(".modal-content").shouldHave(text("NCR Delhi"));
         $("#closeLargeModal").click();
+
+       // sleep(2000);
+
 
     }
 }
