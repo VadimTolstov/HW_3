078cd89b8bf8aaf71da44d1c17cf95da91d1ef1e
VAdimT <vadimt382@gmail.com>
2023-02-24 10:35:39 +0300

    3l

--- src/test/java/CheckingFormTest.java
+++ src/test/java/CheckingFormTest.java
@@ -2,6 +2,11 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -9,7 +14,7 @@ public class CheckingFormTest {
     @BeforeAll
     static void beforeAll(){
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
+//        Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
     }
 
@@ -29,7 +34,35 @@ public class CheckingFormTest {
         $(".react-datepicker__year-dropdown-container").$(byText("1988")).click();
         $("[aria-label='Choose Wednesday, May 25th, 1988']").click();
         $("#subjectsInput").setValue("English").pressEnter();
-        $("#hobbies-checkbox-3").parent().click();
+
+      //  $("#hobbies-checkbox-3").parent().click();
+        $("label[for=hobbies-checkbox-1]").click();
+        $("label[for=hobbies-checkbox-2]").click();
+        $("label[for=hobbies-checkbox-3]").click();
+
+        File file = new File("src/test/resources/pictures/JAVA_20.6_10.jpg");
+        $("#uploadPicture").uploadFile(file);
+
+        $("#currentAddress").setValue("Hello");
+        $("#state").click();
+        $("#react-select-3-option-0").click();
+        $("#city").click();
+        $("#react-select-4-option-0").click();
+
+        $("#submit").click();
+
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $("table tbody tr:nth-child(1) td:nth-child(2)").shouldHave(exactText("Алексей Иванович Ivanov"));
+        $("table tbody tr:nth-child(2) td:nth-child(2)").shouldHave(exactText("Ivanov@google.com"));
+        $("table tbody tr:nth-child(3) td:nth-child(2)").shouldHave(exactText("Female"));
+
+        $(".modal-content").shouldHave(text("7878787878"));
+        $(".modal-content").shouldHave(text("25 May,1988"));
+        $(".modal-content").shouldHave(text("Sports, Reading, Music"));
+        $(".modal-content").shouldHave(text("JAVA_20.6_10.jpg"));
+        $(".modal-content").shouldHave(text("Hello"));
+        $(".modal-content").shouldHave(text("NCR Delhi"));
+        $("#closeLargeModal").click();
 
     }
 }
